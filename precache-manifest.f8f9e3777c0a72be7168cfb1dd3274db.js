8c961204dffbe4a269277472f2aee27dffdb2a37
splin-shady <kirill24007@mail.ru>
2020-04-09 16:27:25 +0300

    Updates

---
+++ precache-manifest.f8f9e3777c0a72be7168cfb1dd3274db.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef246bdef24df2570be2f7fc33f162ac",
+    "url": "/react-first/index.html"
+  },
+  {
+    "revision": "5b77e09fcba7bab8d5ca",
+    "url": "/react-first/static/css/main.f3a7a4d7.chunk.css"
+  },
+  {
+    "revision": "e31906858999f70380fd",
+    "url": "/react-first/static/js/2.4e7c7b8c.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/react-first/static/js/2.4e7c7b8c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5b77e09fcba7bab8d5ca",
+    "url": "/react-first/static/js/main.718eb22a.chunk.js"
+  },
+  {
+    "revision": "524a9ab899961a4f25f2",
+    "url": "/react-first/static/js/runtime-main.8e0a3569.js"
+  },
+  {
+    "revision": "087f5d2ffaf77dec5696b81ca52c63de",
+    "url": "/react-first/static/media/loader.087f5d2f.gif"
+  }
+]);
